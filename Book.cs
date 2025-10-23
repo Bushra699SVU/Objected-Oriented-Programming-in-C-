f5438789f3d56bee9b5bf4bfd639c305272f5b34
sadelorfali-png <sadelorfali@gmail.com>
2025-10-24 01:27:35 +0300

    Add files via upload

---
+++ Book.cs
@@ -0,0 +1,25 @@
+﻿// Book.cs
+using System;
+public class Book : LibraryItem
+{
+    public string Author { get; set; }
+    public string ISBN { get; set; }
+
+    public Book(string title, string author, string isbn) : base(title)
+    {
+        this.Author = author;
+        this.ISBN = isbn;
+    }
+
+    // إعادة تعريف (Override) للدالة المجردة
+    public override void DisplayDetails()
+    {
+        // 
+        Console.WriteLine("--- Book ---");
+        Console.WriteLine($"ID: {UniqueId}");
+        Console.WriteLine($"Title: {Title}");
+        Console.WriteLine($"Author: {Author}");
+        Console.WriteLine($"ISBN: {ISBN}");
+        Console.WriteLine($"Status: {GetStatus()}");
+    }
+}
---
+++ Dvd.cs
@@ -0,0 +1,25 @@
+﻿// Dvd.cs
+using System;
+public class Dvd : LibraryItem
+{
+    public int DurationMinutes { get; set; }
+    public string Director { get; set; }
+
+    public Dvd(string title, string director, int duration) : base(title)
+    {
+        this.Director = director;
+        this.DurationMinutes = duration;
+    }
+
+    // إعادة تعريف (Override) للدالة المجردة
+    public override void DisplayDetails()
+    {
+        // 
+        Console.WriteLine("--- DVD ---");
+        Console.WriteLine($"ID: {UniqueId}");
+        Console.WriteLine($"Title: {Title}");
+        Console.WriteLine($"Director: {Director}");
+        Console.WriteLine($"Duration: {DurationMinutes} mins");
+        Console.WriteLine($"Status: {GetStatus()}");
+    }
+}
