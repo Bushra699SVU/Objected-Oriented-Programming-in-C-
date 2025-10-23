0882945ca6d8bd5ac5ebe0136d3654cd6d21be58
ahmad-rahmoun89 <rahmounahmadtv@gmail.com>
2025-10-24 01:33:30 +0300

    Add files via upload

---
+++ Journal.cs
@@ -0,0 +1,25 @@
+﻿// Journal.cs
+using System;
+public class Journal : LibraryItem
+{
+    public string Publisher { get; set; }
+    public int IssueNumber { get; set; }
+
+    public Journal(string title, string publisher, int issue) : base(title)
+    {
+        this.Publisher = publisher;
+        this.IssueNumber = issue;
+    }
+
+    // إعادة تعريف (Override) للدالة المجردة
+    public override void DisplayDetails()
+    {
+        // 
+        Console.WriteLine("--- Journal ---");
+        Console.WriteLine($"ID: {UniqueId}");
+        Console.WriteLine($"Title: {Title}");
+        Console.WriteLine($"Publisher: {Publisher}");
+        Console.WriteLine($"Issue No: {IssueNumber}");
+        Console.WriteLine($"Status: {GetStatus()}");
+    }
+}
